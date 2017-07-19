a22b785d4110516cd81beded814248c40d3582bc
user <user@DESKTOP-DTRB0Q9>
2017-07-19 19:16:55 +0900

    07-19 주한울 쿼리문 수정

--- GMall/src/main/java/dao/mapper/MemberMapper.java
+++ GMall/src/main/java/dao/mapper/MemberMapper.java
@@ -7,6 +7,6 @@ import logic.Member;
 public interface MemberMapper 
 {
 	@Insert("insert into member (type,id,pass,name,nickname,gender,tel,address,interest,bis_no,bis_name) "
-		  + "values(#{type},#{id},#{pass},#{name},#{nickname},#{gender},#{tel},#{address},#{interest},#{bis_no},#{bis_name}")
+		  + "values(#{type},#{id},#{pass},#{name},#{nickname},#{gender},#{tel},#{address},#{interest},#{bis_no},#{bis_name})")
 	boolean createMember(Member member);
 }
