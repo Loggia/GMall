dad55152ccf5c9d6dd9623aa2d2cfec7a5b6b3b8
JooHanWool <JooHanWool@175.196.216.47>
2017-07-29 16:42:15 +0900

    07-29 주한울 기능 한울서비스로 이동

--- GMall/src/main/java/logic/HanwoolService.java
+++ GMall/src/main/java/logic/HanwoolService.java
@@ -1,7 +1,23 @@
 package logic;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 // 한울이꺼!
 public interface HanwoolService 
 {
-
+	boolean insertMember(Member member); // 주한울 - 회원가입
+	boolean cheakMember(String bis_no); // 주한울 - 회원가입시 중복된 사업자 번호를 사용했는지를 판별
+	Member getUserByIdAndPw(String id, String pass); // 주한울 - 로그인을 위한 메서드
+	void updateMember(Member member, HttpServletRequest request); // 주한울 - 회원 수정
+	void deleteMember(Member member); // 주한울 - 회원 탈퇴
+	List<String> selectBis_no(); // 주한울 - 사업자 번호 확인
+	List<Member> selectBookmark(String id); // 주한울 - 관심 사업자 조회
+	List<Product> selectNewsFeed(String id); // 주한울 - 관심 사업자 최근 등록 목록 조회
+	List<Trade> tradeBuyList(String id); // 주한울 - 일반 회원 구매 목록 확인
+	List<Trade> delvpageBuyList(String id); // 주한울 - 일반 회원 배송 조회
+	List<coupon_history> memberCoupon(String id); // 주한울 일반회원 쿠폰목록
+	List<coupon_history> memberDiscountCheck(String id, String discount); // 주한울 일반회원 쿠폰 관리 선택시
+	List<Trade> moneyChangeList(Member member); // 주한울 일반회원, 사업자 보유금액(금액 변동) 
 }
--- GMall/src/main/java/logic/HanwoolServiceImp.java
+++ GMall/src/main/java/logic/HanwoolServiceImp.java
@@ -1,5 +1,215 @@
 package logic;
 
-public class HanwoolServiceImp {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import dao.BoardDao;
+import dao.MemberDao;
+import dao.ProductDao;
+import dao.TradeDao;
+
+@Service
+public class HanwoolServiceImp 
+{
+	@Autowired
+	BoardDao boardDao;
+	@Autowired
+	MemberDao memberDao;
+	@Autowired
+	TradeDao tradeDao;
+	@Autowired
+	ProductDao productDao;
+	
+	/*
+	 * 주한울
+	 * 회원가입
+	 */
+	@Override
+	public boolean insertMember(Member member) 
+	{
+		return memberDao.insert(member);
+	}
+	
+	/*
+	 * 주한울
+	 * 회원가입시 중복된 사업자 번호를 사용했는지를 판별
+	 */
+	@Override
+	public boolean cheakMember(String bis_no) 
+	{
+		return memberDao.cheakMember(bis_no);
+	}
+	
+	/*
+	 * 주한울
+	 * 로그인
+	 */
+	@Override
+	public Member getUserByIdAndPw(String id, String pass)
+	{
+		return memberDao.getUserByIdAndPw(id, pass);
+	}
+	
+	/*
+	 * 주한울
+	 * 회원 수정
+	 */
+	@Override
+	public void updateMember(Member member, HttpServletRequest request) 
+	{
+		if(member.getPictureFile() != null)
+		{
+			member.setPicture(pictureInsert(member.getId(), member.getPictureFile(), request));
+		}
+		else
+		{
+			member.setPicture("");
+		}
+		
+		memberDao.update(member);
+	}
+	
+	/*
+	 * 주한울 
+	 * 프로필 사진 추가 
+	 */
+	private String pictureInsert(String id, MultipartFile pictureFile, HttpServletRequest request) 
+	{
+		String uploadPath = request.getServletContext().getRealPath("/") + "/prof/";
+		String url = uploadPath + id + "PictureImg";
+		FileOutputStream fos = null;
+		
+		try
+		{
+			fos = new FileOutputStream(url);
+			InputStream in = pictureFile.getInputStream();
+			
+			int data;
+			byte[] buf = new byte[in.available()];
+			
+			while((data = in.read(buf)) != -1)
+			{
+				fos.write(buf,0,data);
+			}
+		}
+		catch(IOException e) 
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if(fos != null)
+				{
+					fos.flush();
+					fos.close();
+				}
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		return id + "PictureImg";
+	}
+	
+	/*
+	 * 주한울
+	 * 회원 탈퇴
+	 */
+	@Override
+	public void deleteMember(Member member) 
+	{
+		memberDao.delete(member);
+	}
+	
+	/*
+	 * 주한울
+	 * 사업자 번호 확인
+	 */
+	@Override
+	public List<String> selectBis_no() 
+	{
+		return memberDao.selectBis_no();
+	}
+	
+	/*
+	 * 주한울
+	 * 관심 사업자 조회
+	 */
+	@Override
+	public List<Member> selectBookmark(String id) 
+	{
+		return memberDao.selectBookmark(id);
+	}
+
+	/*
+	 * 주한울
+	 * 관심 사업자 최근 등록 상품 조회
+	 */
+	@Override
+	public List<Product> selectNewsFeed(String id) 
+	{
+		return productDao.selectNewsFeed(id);
+	}
+	
+	/*
+	 * 주한울
+	 * 일반 회원 구매 목록 확인
+	 */
+	@Override
+	public List<Trade> tradeBuyList(String id) 
+	{
+		return tradeDao.tradeBuyList(id);
+	}
+	
+	/*
+	 * 주한울
+	 * 일반 회원 배송 조회
+	 */
+	@Override
+	public List<Trade> delvpageBuyList(String id) 
+	{
+		return tradeDao.delvpageBuyList(id);
+	}
+	
+	/*
+	 * 주한울
+	 * 일반회원 쿠폰목록
+	 */
+	@Override
+	public List<coupon_history> memberCoupon(String id) 
+	{
+		return tradeDao.memberCoupon(id);
+	}
+	
+	/*
+	 * 주한울
+	 * 일반회원 쿠폰목록 카테고리 선택시
+	 */
+	@Override
+	public List<coupon_history> memberDiscountCheck(String id, String discount) 
+	{
+		return tradeDao.memberDiscountCheck(id,discount);
+	}
+	
+	/*
+	 * 주한울
+	 * 주한울 일반회원, 사업자 보유금액(금액 변동) 
+	 */
+	@Override
+	public List<Trade> moneyChangeList(Member member) 
+	{
+		return tradeDao.moneyChangeList(member);
+	}
 }
