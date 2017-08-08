ae156732ecfd082a0e2d1d6d467828b927f25ec4
JinYoung <JinYoung@DESKTOP-M776MJP>
2017-08-08 14:37:43 +0900

    재수정

--- GMall/src/main/java/dao/MessageDaoImpl.java
+++ GMall/src/main/java/dao/MessageDaoImpl.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-import dao.mapper.BoardMapper;
 import dao.mapper.MessageMapper;
 import logic.Message;
 
+@Repository
 public class MessageDaoImpl implements MessageDao{
 	
 	@Autowired
